0b38c2fa5cd171409fa3ee7fa59bc64b91bfb67f
Lukas Mösle <lukas.moesle@proton.me>
2026-03-13 14:55:15 +0100

    feature: init spring boot app with camunda 7

---
+++ src/main/java/de/lmoesle/processautomationexample/ProcessAutomationExampleApplication.java
@@ -0,0 +1,13 @@
+package de.lmoesle.processautomationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessAutomationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessAutomationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lmoesle/processautomationexample/ProcessAutomationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lmoesle.processautomationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessAutomationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
